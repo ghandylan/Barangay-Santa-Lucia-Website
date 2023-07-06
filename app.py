8b32c0633bcde705d668118f9aceb1da705677ff
Dylan Louis Tayag <dylanlouis.tayag@yahoo.com>
2023-07-06 22:04:38 +0800

    added db connection

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, redirect
 from flask_login import *
-from sqlalchemy import *
-from sqlalchemy.orm import *
-from dotenv import load_dotenv
-import os
+from flask_sqlalchemy import SQLAlchemy
+
+# from models import *
 
 load_dotenv()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') + os.getenv('DATABASE_NAME')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') + '/' + os.getenv('DATABASE_NAME')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
@@ -17,7 +19,6 @@ login_manager.login_view = 'login'
 login_manager.init_app(app)
 
 db = SQLAlchemy(app)
-db.init(app)
 
 
 @app.route('/')
@@ -28,8 +29,9 @@ def hello_world():  # put application's code here
 @app.route('/login')
 def login():
     admin = Admin.query.filter_by(username='admin').first()
-    return 'Login'
+    return admin
 
 
 if __name__ == '__main__':
     app.run()
+    db.create_all()
