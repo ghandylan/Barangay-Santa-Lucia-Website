b7399570dec3cff6cc4aca7a2dc1a5b3510fb90d
Dylan Louis Tayag <dylanlouis.tayag@yahoo.com>
2023-07-06 22:04:46 +0800

    added models

--- models.py
+++ models.py
@@ -1,23 +1,20 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from app import db
-from sqlalchemy import ForeignKey, Integer, String, Boolean
-from sqlalchemy.orm import relationship
 
 
-class Admin(db.Model):
+class Admin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
 
-class BarangayOfficial(db.Model):
+class BarangayOfficial(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     fullname = db.Column(db.String(50), nullable=False)
