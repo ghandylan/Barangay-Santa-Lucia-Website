b88617ec2d87ee45ca780050102e502214ca07f6
Dylan Louis Tayag <dylanlouis.tayag@yahoo.com>
2023-07-07 00:42:48 +0800

    added csrf protection

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, request
 from flask_login import *
+from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
 load_dotenv()
-app = Flask(__name__, template_folder='templates', static_folder='static')
+# initialize the flask app
+app = Flask(__name__)
+
+# configure the database connection
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL') + os.getenv('DATABASE_NAME')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
@@ -16,32 +20,21 @@ login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
+# csrf protection
+csrf = CSRFProtect(app)
+csrf.init_app(app)
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return Admin.query.get(int(user_id))
 
 
-db = SQLAlchemy()
-db.init_app(app)
+with app.app_context():
+    db = SQLAlchemy()
+    db.init_app(app)
 
 
-class Admin(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-
-
-class BarangayOfficial(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
 
 
 @app.route('/')
@@ -54,6 +47,7 @@ def login():
     if request.method == 'POST':
         username = request.form.get("username")
         password = request.form.get("password")
+        # noinspection PyArgumentList
         new_admin = Admin(username=username, password=password)
         db.session.add(new_admin)
         db.session.commit()
