dadfb0cd31216798de6fd8622c262cf431880956
Dylan Louis Tayag <dylanlouis.tayag@yahoo.com>
2023-07-07 00:02:21 +0800

    to be debugged

--- models.py
+++ models.py
@@ -1,21 +1,22 @@
-from flask_login import UserMixin
-from app import db
-
-
-class Admin(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-
-
-class BarangayOfficial(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    fullname = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+# from flask_login import UserMixin
+# from flask_sqlalchemy import SQLAlchemy
+#
+# db = SQLAlchemy()
+#
+#
+# class Admin(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(50), nullable=False)
+#     password = db.Column(db.String(50), nullable=False)
+#
+#
+# class BarangayOfficial(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(50), nullable=False)
+#     password = db.Column(db.String(50), nullable=False)
+#
+#
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(50), nullable=False)
+#     password = db.Column(db.String(50), nullable=False)
