567a7f13c631d6f723bf9f0f769228dc515269c4
Dylan Louis Tayag <dylanlouis.tayag@yahoo.com>
2023-07-08 19:30:00 +0800

    small fix

--- app.py
+++ app.py
@@ -3,25 +3,36 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 
 from config import Config
-from views import *
-
-# initialize the flask app
-app = Flask(__name__)
-app.config.from_object(Config)
-
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.init_app(app)
+from models import db, Admin
+from views import views_blueprint
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return Admin.query.get(int(user_id))
+def create_app():
+    # initialize the flask app
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Admin.query.get(int(user_id))
+
+    # initialize the db
+    migrate = Migrate()
+    # register the db to the migration tool
+    migrate.init_app(app, db)
+    # register the db to the app
+    db.init_app(app)
+
+    app.register_blueprint(views_blueprint)
+
+    return app
 
 
-db.init_app(app)
-migrate = Migrate(app, db)
-init_views(app)
+app_instance = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app_instance.run()
