897d9587221791db061952945d16ca72a6ebfe5a
Dylan Louis Tayag <dylanlouis.tayag@yahoo.com>
2023-07-08 19:35:46 +0800

    added blueprint

--- views.py
+++ views.py
@@ -1,22 +1,23 @@
-from flask import redirect, render_template, request
-
+from flask import redirect, render_template, request, Blueprint
 from models import Admin, db
 
+views_blueprint = Blueprint('views', __name__)
 
-def init_views(app):
-    @app.route('/')
-    def hello_world():  # put application's code here
-        return redirect('/login')
 
-    @app.route('/login', methods=['GET', 'POST'])
-    def login():
-        if request.method == 'POST':
-            username = request.form.get("username")
-            password = request.form.get("password")
-            # noinspection PyArgumentList
-            new_admin = Admin(username=username, password=password)
-            db.session.add(new_admin)
-            db.session.commit()
-            db.session.close()
+@views_blueprint.route('/')
+def hello_world():  # put application's code here
+    return redirect('/login')
 
-        return render_template('login.html')
+
+@views_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # noinspection PyArgumentList
+        new_admin = Admin(username=username, password=password)
+        db.session.add(new_admin)
+        db.session.commit()
+        db.session.close()
+
+    return render_template('login.html')
