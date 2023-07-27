c43ffbdd4f53f99ea4795f8114a7e329f2329838
Dylan Louis Tayag <dylanlouis.tayag@yahoo.com>
2023-07-27 18:54:31 +0800

    removed role column for now

--- models.py
+++ models.py
@@ -4,20 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Admin(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+# how to use flask migrate?
+# 1. flask db init
+# 2. flask db migrate
+# 3. flask db upgrade
 
 
 class BarangayOfficial(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    # role = db.Column(db.String(50), default='barangay_official')
 
 
-class User(db.Model, UserMixin):
+class Resident(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    # role = db.Column(db.String(50), default='resident')
