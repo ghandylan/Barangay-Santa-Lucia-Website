3dbf62fec500aac71a668842d1abf7ed8c67e20e
Your Name <your.email@example.com>
2023-11-04 15:40:05 +0000

    ok

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql://ghandylan2:barangay_db@ghandylan2.mysql.pythonanywhere-services.com/ghandylan2$default"
+    SQLALCHEMY_DATABASE_URI = "mysql://ghandylan2:8AWqNfQ-fWuhSt_@ghandylan2.mysql.pythonanywhere-services.com/ghandylan2$barangay_db"
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') + os.getenv('DATABASE_NAME')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
